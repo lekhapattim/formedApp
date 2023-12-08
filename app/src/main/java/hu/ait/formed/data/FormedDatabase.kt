60a1e0f4740b1660e09decd5e96992b013ddc79c
danielmlampert <daniellampert02@gmail.com>
2023-12-08 15:19:15 +0100

    set up gradle and copied Room stuff

---
+++ app/src/main/java/hu/ait/formed/data/FormedDatabase.kt
@@ -0,0 +1,31 @@
+package hu.ait.formed.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [ShoppingItem::class], version = 1, exportSchema = false)
+abstract class FormedDatabase : RoomDatabase() {
+
+    abstract fun shoppingDao(): ShoppingListDAO
+
+    companion object {
+        @Volatile
+        private var Instance: ShoppingListDatabase? = null
+
+        fun getDatabase(context: Context): ShoppingListDatabase {
+            // if the Instance is not null, return it, otherwise create a new database instance.
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, ShoppingListDatabase::class.java,
+                    "shopping_database.db")
+                    // Setting this option in your app's database builder means that Room
+                    // permanently deletes all data from the tables in your database when it
+                    // attempts to perform a migration with no defined migration path.
+                    .fallbackToDestructiveMigration()
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
+}
