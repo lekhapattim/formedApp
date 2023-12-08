79ca8878be12bb87f42f9c62a59bb29a68a2b955
RebeccaRonai <rebecca.e.ronai.25@dartmouth.edu>
2023-12-08 16:36:41 +0100

    in progress on dance list screen

--- app/src/main/java/hu/ait/formed/screens/AllDances/FormedDanceListScreen.kt
+++ app/src/main/java/hu/ait/formed/screens/AllDances/FormedDanceListScreen.kt
@@ -1,2 +1,13 @@
 package hu.ait.formed.screens.AllDances
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormedDanceListScreen(
+    modifier: Modifier = Modifier,
+    formedDanceListViewModel: FormedDanceListViewModel = hiltViewModel(),
+) {
--- app/src/main/java/hu/ait/formed/screens/AllDances/FormedDanceListViewModel.kt
+++ app/src/main/java/hu/ait/formed/screens/AllDances/FormedDanceListViewModel.kt
@@ -1,4 +1,40 @@
 package hu.ait.formed.screens.AllDances
 
-class FormedDanceListViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import hu.ait.formed.data.Dance
+import hu.ait.formed.data.Form
+import hu.ait.formed.data.FormedDAO
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class FormedDanceListViewModel @Inject constructor(
+    private val formedDAO: FormedDAO
+) : ViewModel() {
+
+    fun getAllDances(): Flow<List<Dance>> {
+        return formedDAO.getAllDances()
+    }
+
+    fun addNewDance(danceItem: Dance) {
+        viewModelScope.launch{
+            formedDAO.insertDance(danceItem)
+        }
+    }
+
+    fun removeDance(danceItem: Dance) {
+        viewModelScope.launch {
+            formedDAO.deleteDance(danceItem)
+        }
+    }
+
+    fun clearAllShopping() {
+        viewModelScope.launch {
+            formedDAO.deleteAllDances()
+        }
+    }
+
 }
