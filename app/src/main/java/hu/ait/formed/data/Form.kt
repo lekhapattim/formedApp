ca4d71dd5c11caa5450af958263f363279123567
danielmlampert <daniellampert02@gmail.com>
2023-12-08 16:34:02 +0100

    one table version

--- app/src/main/java/hu/ait/formed/data/Form.kt
+++ app/src/main/java/hu/ait/formed/data/Form.kt
@@ -5,9 +5,8 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.io.Serializable
 
-@Entity(tableName = "formtable")
 data class Form(
-    @PrimaryKey(autoGenerate = true) var id: Int = 0,
-    @ColumnInfo(name = "title") var title: String,
-    @ColumnInfo(name = "dancers") var dancers: List<Dancer>
+    var id: Int = 0,
+    var title: String,
+    var dancers: List<Dancer>
 ) : Serializable
